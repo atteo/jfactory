1c1cb81b10b0af660f145da0a441e057fd082e4f
Sławek Piotrowski <sentinel@atteo.com>
2016-02-03 23:35:49 +0100

    Use docker-compose instead of a start.sh script

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-trap "exit 1" ERR
-
-mkdir -p review_site
-
-docker build -t jfactory .
-
-docker run -v /home/sentinel/projects/jfactory/review_site:/home/gerrit/review_site -p 8080:8080 -it jfactory
