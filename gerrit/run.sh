9d22fc6908ee6de4251fea5742389fa6836359d8
Sławek Piotrowski <sentinel@atteo.com>
2016-02-09 22:57:22 +0100

    Email configuration for Gerrit

--- gerrit/run.sh
+++ gerrit/run.sh
@@ -4,6 +4,32 @@ gerrit_config() {
 	git config -f etc/gerrit.config "$@"
 }
 
+configure_http() {
+	gerrit_config gerrit.canonicalWebUrl "${PROXY_URL}/gerrit"
+	gerrit_config httpd.listenUrl "proxy-http://*:8080/gerrit/"
+}
+
+configure_ldap() {
+	gerrit_config auth.type "LDAP"
+	gerrit_config ldap.server "${LDAP_URL}"
+	gerrit_config ldap.sslVerify "false"
+	gerrit_config ldap.accountBase "${LDAP_ACCOUNT_BASE}"
+	#gerrit_config ldap.accountScope "one"
+	gerrit_config ldap.groupBase "${LDAP_GROUP_BASE}"
+}
+
+configure_smtp() {
+	if [[ -z "${SMTP_SERVER}" ]]; then
+		gerrit_config sendemail.enable "false"
+		return
+	fi
+
+	gerrit_config sendemail.enable "true"
+	gerrit_config sendemail.smtpServer "${SMTP_SERVER}"
+	gerrit_config sendemail.smtpServerPort "${SMTP_SERVER_PORT:-25}"
+	gerrit_config sendemail.from "${SMTP_FROM}"
+}
+
 echo "Updating..."
 java -jar gerrit.war init -d review_site --batch --no-auto-start
 
@@ -11,15 +37,10 @@ mv plugins/* review_site/plugins/
 
 cd review_site
 
-gerrit_config gerrit.canonicalWebUrl "${PROXY_URL}/gerrit"
-gerrit_config httpd.listenUrl "proxy-http://*:8080/gerrit/"
+configure_http
+configure_ldap
+configure_smtp
 
-gerrit_config auth.type "LDAP"
-gerrit_config ldap.server "${LDAP_URL}"
-gerrit_config ldap.sslVerify "false"
-gerrit_config ldap.accountBase "${LDAP_ACCOUNT_BASE}"
-#gerrit_config ldap.accountScope "one"
-gerrit_config ldap.groupBase "${LDAP_GROUP_BASE}"
 
 
 echo "Reindexing..."
