c188499b2ea3a59ba3f851f0f19fca4078c0ecdc
Sławek Piotrowski <sentinel@atteo.com>
2016-06-14 23:37:40 +0200

    Scripts to build and run the slave

---
+++ slave/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker run -v /etc/localtime:/etc/localtime:ro \
+	-v /etc/ssl/certs:/etc/ssl/certs:ro \
+	-v /usr/share/ca-certificates/:/usr/share/ca-certificates/:ro \
+	-v /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/security:/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/security:ro \
+	-ti jfactory_slave
+
