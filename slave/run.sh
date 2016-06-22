a6b2b8384e3c32aa04d1dd11da664880e9d7abfe
Sławek Piotrowski <sentinel@atteo.com>
2016-06-23 00:44:52 +0200

    Cache .m2/repository of slaves in a separate Docker Volume

--- slave/run.sh
+++ slave/run.sh
@@ -4,5 +4,6 @@ docker run -v /etc/localtime:/etc/localtime:ro \
 	-v /etc/ssl/certs:/etc/ssl/certs:ro \
 	-v /usr/share/ca-certificates/:/usr/share/ca-certificates/:ro \
 	-v /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/security:/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/security:ro \
+	-v jfactory_slave-m2-repository:/home/jenkins/.m2/repository \
 	-ti jfactory_slave
 
