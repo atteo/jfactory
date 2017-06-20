8426ed07b0711f8553ee5b55cfd747a8d55de3c9
Sławek Piotrowski <sentinel@atteo.com>
2017-06-20 11:26:17 +0200

    Use polling in batchuser-gerrit-plugin
    Previously batch user was set up after first project was created.
    As there is no event in Gerrit to detect log in of the user,
    this plugin checks each 2 seconds whether first user has logged in.

--- batchuser-gerrit-plugin/src/main/java/org/atteo/jfactory/CreateBatchUser.java
+++ batchuser-gerrit-plugin/src/main/java/org/atteo/jfactory/CreateBatchUser.java
@@ -1,43 +1,50 @@
 package org.atteo.jfactory;
 
-import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-
-import javax.inject.Inject;
-
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.google.gerrit.common.TimeUtil;
 import com.google.gerrit.common.data.GroupDescriptions;
-import com.google.gerrit.reviewdb.client.*;
+import com.google.gerrit.common.errors.InvalidSshKeyException;
+import com.google.gerrit.extensions.annotations.PluginName;
+import com.google.gerrit.extensions.restapi.UnprocessableEntityException;
+import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.reviewdb.client.AccountExternalId;
+import com.google.gerrit.reviewdb.client.AccountGroup;
+import com.google.gerrit.reviewdb.client.AccountGroupMember;
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.IdentifiedUser;
 import com.google.gerrit.server.account.AccountByEmailCache;
 import com.google.gerrit.server.account.AccountCache;
 import com.google.gerrit.server.account.AccountLoader;
 import com.google.gerrit.server.account.VersionedAuthorizedKeys;
+import com.google.gerrit.server.config.PluginConfig;
+import com.google.gerrit.server.config.PluginConfigFactory;
+import com.google.gerrit.server.git.WorkQueue;
 import com.google.gerrit.server.group.GroupsCollection;
+import com.google.gerrit.server.ssh.SshKeyCache;
+import com.google.gerrit.server.util.RequestContext;
+import com.google.gerrit.server.util.ThreadLocalRequestContext;
+import com.google.gwtorm.server.OrmException;
+import com.google.gwtorm.server.SchemaFactory;
+import com.google.inject.Provider;
 import org.eclipse.jgit.errors.ConfigInvalidException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.collect.Lists;
-import com.google.gerrit.common.errors.InvalidSshKeyException;
-import com.google.gerrit.extensions.annotations.PluginName;
-import com.google.gerrit.extensions.events.NewProjectCreatedListener;
-import com.google.gerrit.extensions.restapi.UnprocessableEntityException;
-import com.google.gerrit.reviewdb.server.ReviewDb;
-import com.google.gerrit.server.config.PluginConfig;
-import com.google.gerrit.server.config.PluginConfigFactory;
-import com.google.gerrit.server.ssh.SshKeyCache;
-import com.google.gwtorm.server.OrmException;
-import com.google.gwtorm.server.SchemaFactory;
+import javax.inject.Inject;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
-public class CreateBatchUser implements NewProjectCreatedListener {
+public class CreateBatchUser {
 	private final Logger logger = LoggerFactory.getLogger(CreateBatchUser.class);
 	private final SchemaFactory<ReviewDb> schema;
 	private final PluginConfig pluginConfig;
 	private final SshKeyCache sshKeyCache;
-    private final VersionedAuthorizedKeys.Accessor authorizedKeys;
+	private final VersionedAuthorizedKeys.Accessor authorizedKeys;
 	private final AccountCache accountCache;
 	private final AccountByEmailCache byEmailCache;
 	private final AccountLoader.Factory infoLoader;
@@ -45,56 +52,80 @@ public class CreateBatchUser implements NewProjectCreatedListener {
 	private final String username;
 	private final String name;
 	private final String sshKey;
+	private final WorkQueue.Executor executor;
+	private final ThreadLocalRequestContext context;
+	private final IdentifiedUser.GenericFactory userFactory;
 
 	@Inject
 	public CreateBatchUser(@PluginName String pluginName, SchemaFactory<ReviewDb> schema,
-			PluginConfigFactory configFactory, SshKeyCache sshKeyCache, VersionedAuthorizedKeys.Accessor authorizedKeys,
-        	AccountCache accountCache, AccountByEmailCache byEmailCache, AccountLoader.Factory infoLoader,
-			GroupsCollection groupsCollection) {
+						   PluginConfigFactory configFactory, SshKeyCache sshKeyCache, VersionedAuthorizedKeys.Accessor authorizedKeys,
+						   AccountCache accountCache, AccountByEmailCache byEmailCache, AccountLoader.Factory infoLoader,
+						   GroupsCollection groupsCollection, WorkQueue queue, ThreadLocalRequestContext context,
+						   IdentifiedUser.GenericFactory userFactory) {
 		this.schema = schema;
 		this.sshKeyCache = sshKeyCache;
-        this.authorizedKeys = authorizedKeys;
+		this.authorizedKeys = authorizedKeys;
 		this.accountCache = accountCache;
 		this.byEmailCache = byEmailCache;
 		this.infoLoader = infoLoader;
 		this.groupsCollection = groupsCollection;
+		this.context = context;
+		this.userFactory = userFactory;
 		pluginConfig = configFactory.getFromGerritConfig(pluginName);
 		username = pluginConfig.getString("username", "jenkins");
 		sshKey = pluginConfig.getString("sshKey");
 		name = pluginConfig.getString("name", "Batch user");
+		executor = queue.getDefaultQueue();
 
-		createBatchUserIfNotExistsYet();
-	}
-
-	@Override
-	public void onNewProjectCreated(Event event) {
 		createBatchUserIfNotExistsYet();
 	}
 
 	private void createBatchUserIfNotExistsYet() {
 		try (ReviewDb db = schema.open()) {
-			if (db.accounts().anyAccounts().toList().isEmpty()) {
-				logger.info("Cannot create batch user account as an admin account, create admin user first");
+			List<Account> accounts = db.accounts().anyAccounts().toList();
+			if (accounts.isEmpty()) {
+				logger.info("Cannot create batch user account. No admin user yet. Log in as admin user to Gerrit.");
+				executor.schedule(this::createBatchUserIfNotExistsYet, 2, TimeUnit.SECONDS);
 				return;
 			}
 
-			AccountExternalId.Key accountKey = new AccountExternalId.Key(AccountExternalId.SCHEME_USERNAME, username);
-			AccountExternalId accountExternalId = db.accountExternalIds().get(accountKey);
+			RequestContext oldContext = null;
 
-			if (accountExternalId == null) {
-				accountExternalId = createBatchUser(db);
-				logger.info("Batch user account created");
+			try {
+				oldContext = context.setContext(new RequestContext() {
+					@Override
+					public CurrentUser getUser() {
+						return userFactory.create(accounts.iterator().next().getId());
+					}
+
+					@Override
+					public Provider<ReviewDb> getReviewDbProvider() {
+						return () -> db;
+					}
+				});
+
+				AccountExternalId.Key accountKey = new AccountExternalId.Key(AccountExternalId.SCHEME_USERNAME, username);
+				AccountExternalId accountExternalId = db.accountExternalIds().get(accountKey);
+
+				if (accountExternalId == null) {
+					accountExternalId = createBatchUser(db);
+					logger.info("Batch user account created");
+				}
+
+				authorizedKeys.addKey(accountExternalId.getAccountId(), sshKey);
+				sshKeyCache.evict(username);
+			} finally {
+				if (oldContext != null) {
+					context.setContext(oldContext);
+				}
 			}
-
-			authorizedKeys.addKey(accountExternalId.getAccountId(), sshKey);
-			sshKeyCache.evict(username);
-		} catch (OrmException|IOException|ConfigInvalidException|InvalidSshKeyException e) {
+		} catch (OrmException | IOException | ConfigInvalidException | InvalidSshKeyException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
 	private AccountExternalId createBatchUser(ReviewDb db) {
-		logger.info("Creating new batch user");
+		logger.info("Creating new batch user: " + name);
 
 		try {
 			return createNewUser(db);
@@ -106,8 +137,8 @@ public class CreateBatchUser implements NewProjectCreatedListener {
 	private AccountExternalId createNewUser(ReviewDb db) throws OrmException, UnprocessableEntityException {
 		Account.Id id = new Account.Id(db.nextAccountId());
 		AccountExternalId extUser =
-				new AccountExternalId(id, new AccountExternalId.Key(
-						AccountExternalId.SCHEME_USERNAME, username));
+			new AccountExternalId(id, new AccountExternalId.Key(
+				AccountExternalId.SCHEME_USERNAME, username));
 
 		/*
 		if (input.httpPassword != null) {
@@ -124,7 +155,7 @@ public class CreateBatchUser implements NewProjectCreatedListener {
 
 		for (AccountGroup.Id groupId : groups) {
 			AccountGroupMember m =
-					new AccountGroupMember(new AccountGroupMember.Key(id, groupId));
+				new AccountGroupMember(new AccountGroupMember.Key(id, groupId));
 			db.accountGroupMembers().insert(Collections.singleton(m));
 		}
 
@@ -134,12 +165,12 @@ public class CreateBatchUser implements NewProjectCreatedListener {
 	}
 
 	private Set<AccountGroup.Id> parseGroups(List<String> groups)
-			throws UnprocessableEntityException {
+		throws UnprocessableEntityException {
 		Set<AccountGroup.Id> groupIds = Sets.newHashSet();
 		if (groups != null) {
 			for (String g : groups) {
 				groupIds.add(GroupDescriptions.toAccountGroup(
-						groupsCollection.parseInternal(g)).getId());
+					groupsCollection.parseInternal(g)).getId());
 			}
 		}
 		return groupIds;
--- batchuser-gerrit-plugin/src/main/java/org/atteo/jfactory/CreateBatchUserModule.java
+++ batchuser-gerrit-plugin/src/main/java/org/atteo/jfactory/CreateBatchUserModule.java
@@ -1,14 +1,10 @@
 package org.atteo.jfactory;
 
-import com.google.gerrit.extensions.events.NewProjectCreatedListener;
-import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.inject.AbstractModule;
 
 public class CreateBatchUserModule extends AbstractModule {
 	@Override
 	protected void configure() {
 		bind(CreateBatchUser.class).asEagerSingleton();
-		DynamicSet.bind(binder(), NewProjectCreatedListener.class)
-				.to(CreateBatchUser.class);
 	}
 }
