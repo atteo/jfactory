1a30318a95cbeccb72a8f1da07ad857f39c52109
Bartosz Bełcik <bartosz.belcik@ecom.software>
2017-06-22 10:19:06 +0200

    docker-slave merged into cloud-slave

--- images/docker-slave/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-trap "exit 1" ERR
-
-# go to the script directory
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-
-docker build --build-arg=http_proxy --build-arg=https_proxy --build-arg=no_proxy -t jfactory/docker-slave:2.0 .
--- images/docker-slave/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run -v /etc/localtime:/etc/localtime:ro \
-	-v /etc/ssl/certs:/etc/ssl/certs:ro \
-	-v /usr/share/ca-certificates/:/usr/share/ca-certificates/:ro \
-	-v /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/security:/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/security:ro \
-	-ti jfactory/docker-slave:1.0
-
