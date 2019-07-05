45a0341c4a521fa4ae429627f9fbc69731ebd19a
Sławek Piotrowski <sentinel@atteo.com>
2019-07-05 02:08:26 +0200

    Update to Gerrit 3.0

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ trap "exit 1" ERR
 # go to the script directory
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-cd batchuser-gerrit-plugin
-mvn clean install
-cd ..
+#cd batchuser-gerrit-plugin
+#mvn clean install
+#cd ..
 
 images/build.sh "${1:-latest}"
 
